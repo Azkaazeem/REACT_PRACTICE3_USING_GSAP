60d01b4febd6e5fe796306ef18df5a7811c33661
Azkka azeem <azkaazeem804@gmail.com>
2026-03-24 08:04:03 +0500

    Done GSAP animation and Logic

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// import { useGSAP } from '@gsap/react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React, { useRef } from 'react'
+
+const App = () => {
+
+  const boxRef = useRef()
+
+  const { contextSafe } = useGSAP()
+
+  const rotateBox = contextSafe(() => {
+    gsap.to(boxRef.current, {
+      rotate: 360,
+      duration: 1
+    })
+  })
+
+  return (
+    <main>
+      <button onClick={rotateBox}>Animate</button>
+      <div ref={boxRef} className="box">Box</div>
+    </main>
+  )
+}
+
+export default App
